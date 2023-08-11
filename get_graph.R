dd60d4fe87f889010e0310e91fff271d00f2a2fa
Alvaro Ignacio Tapia <119822693+alvaroitapia@users.noreply.github.com>
2023-08-11 10:04:19 +0100

    Add files via upload

--- get_graph.R
+++ get_graph.R
@@ -1,24 +1,31 @@
 
 get_graph <- function(data) {
 
-  # Clean data
-  complete_data <- data[complete.cases(data[, c('%VAR.', '%RATE', 'ISSUER NAME')]), ]
-  c4 <- as.numeric(complete_data$'%VAR.')
-  c5 <- as.numeric(complete_data$'%RATE')
+  tryCatch({
+    # Clean data
+    complete_data <- data[complete.cases(data[, c('%VAR.', '%RATE', 'ISSUER NAME')]), ]
+    x <- as.numeric(complete_data$'%VAR.')
+    y <- as.numeric(complete_data$'%RATE')
 
-  # Plot data
-  plot(c5, c4, pch = 16, cex = 1, col = "purple",
-       xlim = c(min(c5), max(c5) * 1.1),
-       ylim = c(min(c4), max(c4) * 1.1),
-       xlab = '%VAR.', ylab = '%RATE',
-       main = "Correlation: %RATE and %VAR. for all securities")
+    # Plot data
+    plot(x, y, pch = 16, cex = 1, col = "purple",
+         xlim = c(min(x), max(x) * 1.1),
+         ylim = c(min(y), max(y) * 1.1),
+         xlab = '%VAR.', ylab = '%RATE',
+         main = "Correlation: %RATE and %VAR. for all securities")
 
-  text(c5, c4, labels = complete_data$`ISSUER NAME`, pos = 3, col = "black", cex = 0.68)
+    text(x, y, labels = complete_data$`ISSUER NAME`, pos = 3, col = "black", cex = 0.68)
 
-  # Correlation line & coefficient
-  correlation <- cor(c5, c4)
-  abline(lm(c4 ~ c5), col = "red")
-  text(0.8 * max(c5), 0.9 * max(c4), paste("Coefficient:",
+    # Correlation line & coefficient
+    correlation <- cor(x, y)
+    abline(lm(y ~ x), col = "red")
+    text(0.8 * max(x), 0.9 * max(y), paste("Coefficient:",
                                            round(correlation, 2)), col = "red")
-  return()
+  }, error = function(e) {
+    message('Apologies. The get_graph function does not accept NULL values.')
+  })
 }
+
+
+
+
